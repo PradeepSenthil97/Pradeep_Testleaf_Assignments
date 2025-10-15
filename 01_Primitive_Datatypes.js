9012be1740ee73c23302b53484800c294bc03b50
PradeepSenthil97 <95753273+PradeepSenthil97@users.noreply.github.com>
2025-10-15 23:10:49 +0530

    Add files via upload

---
+++ 01_Primitive_Datatypes.js
@@ -0,0 +1,17 @@
+var firstName="Pradeep"
+var companyName='RNT'
+var mobileNumber= 98273493284
+var isAutomation = true
+var hasPlaywright
+
+console.log("My Name is :" + " " +firstName)
+console.log("My Company Name is :" + " " +companyName)
+console.log("My Mobile Number is :" + " " +mobileNumber)
+console.log("Automation Enabled :" + " " +isAutomation)
+console.log("Playwright Enabled :" + " "+hasPlaywright)
+
+console.log("Type of My Name is :" + " " +typeof(firstName))
+console.log("Type of My Company Name is :" + " " +typeof(companyName))
+console.log("Type of My Mobile Number is :" + " " +typeof(mobileNumber))
+console.log("Type of Automation Enabled :" + " " +typeof(isAutomation))
+console.log("Type of Playwright Enabled :" + " "+typeof(hasPlaywright))
